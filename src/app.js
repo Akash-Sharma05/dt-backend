e833d25959a27d1f89ef5d2fec3a975de4355408
Akash new <akashladwa64.in>
2026-09-09 11:27:34 +0530

    api-data

--- src/app.js
+++ src/app.js
@@ -1,24 +1,103 @@
 const express = require('express')
 const app = express();
 const dbConnect = require("./config/database")
-const User = require("./models/user")
+const User = require("./models/user");
+
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-    //create a new instance of the user model
-    const user = new User({
-        firstName: "rohit",
-        lastName: "sharma",
-        emailId: "rohit@gmail.com",
-        password: "rohit@123"
-    })
     try {
-        await user.save();
-        res.send("User created successfully..")
-    }catch(err){
-        res.status(400).send("Error saving the user : " + err.message)
+        const { firstName, lastName, emailId, password, gender } = req.body;
+        const user = await User.create({
+            firstName, lastName, emailId, password, gender
+        })
+        await user.save()
+        return res.status(200).send({ message: "User created successfully", user });
+    } catch (err) {
+        return res.status(500).send("ERROR : " + err)
     }
+    // console.log(req.body)
 
 })
+
+//get user by email 
+app.get("/user", async (req, res) => {
+    try {
+        const ID = req.body._id;
+        console.log(ID)
+
+        const user = await User.findById(ID)
+        if (!user) {
+            return res.status(400).send({
+                message: "User is not available"
+            })
+        }
+        return res.status(200).send({
+            message: "User fetched successfully",
+            user
+        })
+
+    } catch (err) {
+        return res.status(500).send("ERROR : " + err)
+    }
+})
+
+//get all the users
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find()
+        if (users.length == 0) {
+            return res.status(404).send({
+                message: "User not found"
+            })
+        }
+        return res.status(200).send({
+            message: "User fetched successfully",
+            users
+        })
+
+    } catch (err) {
+        return res.status(500).send("ERROR : " + err)
+    }
+})
+
+//to delete the user
+app.delete("/delete", async (req, res) => {
+    const id = req.body._id
+    try {
+        const user = await User.findByIdAndDelete(id);
+
+        return res.status(400).send("User Deleted successfully...")
+
+
+    } catch (err) {
+        return res.status(500).send("ERROR : " + err)
+    }
+})
+
+//find by id and update 
+app.patch("/user/:userId", async (req, res) => {
+    const ID = req.params?.userId
+    const data = req.body;
+    try {
+        const ALLOWED_UPDATES = ["password", "photoUrl", "about", "gender", "age", "skills"]
+
+        const isUpadteAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k))
+        if (!isUpadteAllowed) {
+            throw new Error("Update not allowed ")
+        }
+        if(data.skills.length>10){
+             throw new Error("Skills cannot be more than 10 ")
+        }
+
+        await User.findByIdAndUpdate(ID, data)
+        return res.status(400).send("User updated successfully")
+
+    } catch (err) {
+        return res.status(500).send("ERROR : " + err)
+    }
+})
+
 
 dbConnect().then(() => {
     console.log("Databse connection established...")
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,59 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose');
+
 const userSchema = mongoose.Schema({
-    firstName:{
-        type:String,
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 40,
     },
-    lastName:{
-        type:String
-    },
-    emailId:{
-        type:String
-    },
-    password:{
-        typed:Number
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type:String
-    }
-})
 
+    lastName: {
+        type: String
+    },
 
-module.exports= mongoose.model("User",userSchema)
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        min: 18,
+    },
+
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+
+    photoUrl: {
+        type: String,
+        default: "https://w7.pngwing.com/pngs/910/606/png-transparent-head-the-dummy-avatar-man-tie-jacket-user-thumbnail.png"
+    },
+    about: {
+        type: String,
+        default: "This is default user photo"
+    },
+    skills: {
+        type: [String]
+    },
+   
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User
